948ae947edb07d93ae4b05edd6fecb999c1b39e9
Peter Galonza <p.galonza@evaron.ru>
2023-09-30 14:58:34 +0300

    update test for deb distro

--- extensions/molecule/bash_profile/tests/test_general.py
+++ extensions/molecule/bash_profile/tests/test_general.py
@@ -5,6 +5,14 @@ def test_massage_file(host):
     assert massage.mode == 0o755
 
 def test_profile_file(host):
+    distro_name = host.system_info.distribution
+    print(distro_name)
     user_home = host.user().home
-    profile = host.file(user_home + "/.bash_profile")
+
+    if distro_name in ['debian', 'ubuntu']:
+        profile_name = '.profile'
+    elif distro_name in ['redhat', 'almalinux']:
+        profile_name = '.bash_profile'
+
+    profile = host.file(user_home + "/" + profile_name)
     assert profile.contains("PS1")
