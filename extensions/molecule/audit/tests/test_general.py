5aff81b96788b382db5810509726eeda1a97c860
Peter Galonza <p.galonza@evaron.ru>
2024-06-21 09:17:46 +0300

    removed test, problem with get_variables

--- extensions/molecule/audit/tests/test_general.py
+++
@@ -1,6 +0,0 @@
-def lynis_install(host):
-    lynis_version = host.ansible.get_variables()["lynis_version"]
-    assert host.file("/tmp/lynis-" + lynis_version).exists
-
-def lynis_report(host):
-    assert host.file("/var/log/lynis-report.dat").exists
